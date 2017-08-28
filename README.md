6eee3d97eb3bd46a3205450656af17c56b5f8800
zhengbaofeng <200575974@qq.com>
2017-08-28 15:35:43 +0800

    代码建仓

---
+++ README.md
@@ -0,0 +1,2 @@
+##自建node            服务文件夹
+	--service文件夹   自建VPS Node测试服务 
---
+++ service/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {
+  console.log("发送成功")
+  res.send({ msg: '返回成功' })
+});
+
+module.exports = router;
